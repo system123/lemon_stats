b28e32efe90a29d0ea23cb3e45344a23c846449e
Lloyd Hughes <lloyd@enigmasoft.co.za>
2016-05-30 21:05:02 +0200

    Fixed module names and class names

--- lib/lemon_stats.rb
+++ lib/lemon_stats.rb
@@ -7,5 +7,41 @@ require "lemon_stats/version"
 ).each { |f| require "lemon_stats/#{f}" }
 
 class LemonStats
-  # Your code goes here...
+  	attr_accessor :store_collection
+	attr_accessor :stats_group
+	attr_accessor :base_key
+
+	def intialize(base_endpoint)
+		@base_key = base_endpoint
+		@store_collection = LemonStats::StoreCollection.new @base_key
+		@stats_group = LemonStats::StatsGroup.new @base_key, @store_collection
+	end
+
+	def add_store(store)
+		@store_collection.add store
+	end
+
+	def remove_store(store)
+		@store_collection.remove store
+	end
+
+	def add_stats_group(group)
+		@stats_group.add_sub_group group
+	end
+
+	def remove_stats_group(group)
+		@stats_group.remove_sub_group group
+	end
+
+	def find_group_by_name(group)
+
+	end
+
+	def add_stat(stat, key)
+
+	end
+
+	def remove_stat(stat, key)
+
+	end
 end
